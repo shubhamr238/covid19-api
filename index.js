c2f6e8117b6e8cb17780d56a66c750dc55e22f1c
shubhamr238 <shubhamr238@gmail.com>
2021-04-19 14:17:45 +0530

    update packages and add cors

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const db = require("./config/mongoose");
-const port = 8000;
+const port = process.env.port || 8000;
 
+//use cros
+app.use(cors());
 
 // body parser for req.body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // use express router
 app.use("/", require("./routes"));
 
 //Server Listner
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (err) {
     console.log("Error Running the Server", err);
   }
